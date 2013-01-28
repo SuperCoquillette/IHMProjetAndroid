333696bf1ad06e07cfe7a77e2dbb93bc37731390
b21100041 <b21100041@172.17.106.31>
2013-01-28 14:38:41 +0100

    1er commit

---
+++ FamilleApplication/src/univ/amu/familleApplication/AgendaActivity.java
@@ -0,0 +1,22 @@
+package univ.amu.familleApplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AgendaActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_agenda);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_agenda, menu);
+		return true;
+	}
+
+}
